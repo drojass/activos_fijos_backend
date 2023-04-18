772d4647ec84a1eb0a3d5bcde1a66bae02f07806
Diego Rojas <drojas@grupoasd.com>
2023-04-18 00:16:02 -0500

    chore: Se modifican los archivos de configuración del proyecto.

--- src/main/java/co/com/grupoasd/prueba/activos/activosfijos/ActivosfijosApplication.java
+++ src/main/java/co/com/grupoasd/prueba/activos/activosfijos/ActivosfijosApplication.java
@@ -1,13 +1,79 @@
+/*
+ * Archivo: ActivosfijosApplication.java
+ * Fecha: 2023-04-17
+ * Todos los derechos de propiedad intelectual e industrial sobre esta
+ * aplicacion son de propiedad exclusiva del GRUPO ASD S.A.S.
+ * Su uso, alteracion, reproduccion o modificacion sin el debido
+ * consentimiento por escrito de GRUPO ASD S.A.S. quedan totalmente prohibidos.
+ *
+ * Este programa se encuentra protegido por las disposiciones de la
+ * Ley 23 de 1982 y demas normas concordantes sobre derechos de autor y
+ * propiedad intelectual. Su uso no autorizado dara lugar a las sanciones
+ * previstas en la Ley.
+ */
 package co.com.grupoasd.prueba.activos.activosfijos;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.Properties;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
+@RestController
 @SpringBootApplication
 public class ActivosfijosApplication {
+    private static final TimeZone DEFAULT_TIME_ZONE = TimeZone.getTimeZone("America/Bogota");
+    @Autowired
+    BuildProperties buildProperties;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActivosfijosApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(DEFAULT_TIME_ZONE);
+        log.info("Spring boot application running in UTC timezone :" + new Date());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivosfijosApplication.class, args);
+    }
+
+    /**
+     * Definición del Bean que permite realizar la conexión a otros servicios.
+     * RestFull
+     *
+     * @return rest template.
+     */
+    @Bean
+    public RestTemplate rest() {
+        return new RestTemplate();
+    }
+
+    /**
+     * Servicio rest que permite consultar las propiedades del proyecto.
+     *
+     * @return BuildProperties
+     */
+    @GetMapping(value = "/about", produces = "application/json")
+    public Properties about() {
+        Properties prop = new Properties();
+        /*
+         * need to explicitly loop over all entries, just returning the
+         * BuildProperties object only contains the specific fields (artificact,
+         * group, name, time and version)
+         */
+        buildProperties.forEach(entry -> prop.put(entry.getKey(), entry.getValue()));
+        //proper date formatting for time
+        prop.put("time", Instant.ofEpochMilli(Long.parseLong(prop.getProperty("time"))).toString());
+        return prop;
+    }
 
 }
---
+++ src/main/java/co/com/grupoasd/prueba/activos/activosfijos/config/SwaggerConfig.java
@@ -0,0 +1,70 @@
+/*
+ * Archivo: SwaggerConfig.java
+ * Fecha: 2023-04-17
+ * Todos los derechos de propiedad intelectual e industrial sobre esta
+ * aplicacion son de propiedad exclusiva del GRUPO ASD S.A.S.
+ * Su uso, alteracion, reproduccion o modificacion sin el debido
+ * consentimiento por escrito de GRUPO ASD S.A.S. quedan totalmente prohibidos.
+ *  
+ * Este programa se encuentra protegido por las disposiciones de la
+ * Ley 23 de 1982 y demas normas concordantes sobre derechos de autor y
+ * propiedad intelectual. Su uso no autorizado dara lugar a las sanciones
+ * previstas en la Ley.
+ */
+package co.com.grupoasd.prueba.activos.activosfijos.config;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Bean que instancia la configuración de Swagger.
+ *
+ * @author Diego Alejandro Rojas Suárez drojasm@grupoasd.com
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${application.contact.name}")
+    private String contactName;
+    @Value("${application.contact.url}")
+    private String contactUrl;
+    @Value("${application.contact.email}")
+    private String contactEmail;
+    @Value("${application.info.api.title}")
+    private String infoApiTitle;
+    @Value("${application.info.api.description}")
+    private String infoApiDescription;
+    @Value("${application.info.api.version}")
+    private String infoApiVersion;
+    @Value("${application.info.api.termsOfServiceUrl}")
+    private String infoApiTermOfServiceUrl;
+    @Value("${application.info.api.license}")
+    private String infoApiLicense;
+    @Value("${application.info.api.licenseUrl}")
+    private String infoApiLicenseUrl;
+
+    /**
+     * Bean de clase.
+     *
+     * @return docket.
+     */
+    @Bean
+    public Docket api(BuildProperties buildProperties) {
+        infoApiTitle = buildProperties.getName() + " - " + infoApiTitle;
+        infoApiVersion = buildProperties.getVersion();
+        Contact defaultConcat = new Contact(contactName, contactUrl, contactEmail) {
+        };
+
+        ApiInfo defaultApiInfo = new ApiInfo(infoApiTitle, infoApiDescription, infoApiVersion, infoApiTermOfServiceUrl,
+                defaultConcat, infoApiLicense, infoApiLicenseUrl, new ArrayList<>());
+
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(defaultApiInfo);
+    }
+}
