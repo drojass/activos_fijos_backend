76c9ef715739cf947cd7ff747d236ae28d17eb2c
Diego Rojas <drojas@grupoasd.com>
2023-04-25 22:08:10 -0500

    feat: Se agregan las entidades de las colecciones del modelo de datos de activos fijos.

---
+++ src/main/java/co/com/grupoasd/prueba/activos/activosfijos/entity/ActivoFijo.java
@@ -0,0 +1,44 @@
+/*
+ * Archivo: ActivoFijo.java
+ * Fecha: 2022-04-19
+ * Todos los derechos de propiedad intelectual e industrial sobre esta
+ * aplicacion son de propiedad exclusiva del GRUPO ASD S.A.S.
+ * Su uso, alteracion, reproduccion o modificacion sin el debido
+ * consentimiento por escrito de GRUPO ASD S.A.S. quedan totalmente prohibidos.
+ *
+ * Este programa se encuentra protegido por las disposiciones de la
+ * Ley 23 de 1982 y demas normas concordantes sobre derechos de autor y
+ * propiedad intelectual. Su uso no autorizado dara lugar a las sanciones
+ * previstas en la Ley.
+ */
+package co.com.grupoasd.prueba.activos.activosfijos.entity;
+
+import java.util.Date;
+import java.util.List;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Entidad para la colección de Tipo de activos.
+ *
+ * @author Diego Alejandro Rojas Suárez drojas@grupoasd.com
+ */
+@Document(collection = "ACTIVO_FIJO")
+@Data
+public class ActivoFijo {
+
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private List<SubActivo> subActivos;
+    private String serial;
+    private String placa;
+    private String peso;
+    private String alto;
+    private String ancho;
+    private String largo;
+    private Integer valorCompra;
+    private Date fechaCompra;
+    private Date fechaBaja;
+    private Boolean activo;
+}
---
+++ src/main/java/co/com/grupoasd/prueba/activos/activosfijos/entity/Categoria.java
@@ -0,0 +1,30 @@
+/*
+ * Archivo: Categoria.java
+ * Fecha: 2022-04-19
+ * Todos los derechos de propiedad intelectual e industrial sobre esta
+ * aplicacion son de propiedad exclusiva del GRUPO ASD S.A.S.
+ * Su uso, alteracion, reproduccion o modificacion sin el debido
+ * consentimiento por escrito de GRUPO ASD S.A.S. quedan totalmente prohibidos.
+ *
+ * Este programa se encuentra protegido por las disposiciones de la
+ * Ley 23 de 1982 y demas normas concordantes sobre derechos de autor y
+ * propiedad intelectual. Su uso no autorizado dara lugar a las sanciones
+ * previstas en la Ley.
+ */
+package co.com.grupoasd.prueba.activos.activosfijos.entity;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Entidad para la colección de las categorías de cada tipo de activo.
+ *
+ * @author Diego Alejandro Rojas Suárez drojas@grupoasd.com
+ */
+@Document(collection = "CATEGORIA")
+@Data
+public class Categoria {
+
+    private Integer id;
+    private String descripcion;
+}
